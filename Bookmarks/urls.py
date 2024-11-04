fe5138e30ae349040575fbc24fc3de6ec5892695
Wilkolak3 <patrykjanusz@onet.pl>
2024-11-04 12:19:29 +0100

    Kod z 04.11

--- Bookmarks/urls.py
+++ Bookmarks/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+
+from Bookmarks import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('Account.urls')),
 ]
+
+# jezeli jest debug to rozszerz liste o ten zapis
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
